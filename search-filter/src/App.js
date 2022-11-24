b83e57d1d7b412c873b929f71a544c2bb8c6743f
Deep-Ramani <deepramani8505@gmail.com>
2022-11-24 18:43:55 +0530

    Add Functionality

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const list = [
+    'Banana',
+    'Apple',
+    'Orange',
+    'Mango',
+    'Pineapple',
+    'Watermelon',
+  ];
+
+  const [filterList, setFilterList] = useState(list);
+
+  const handleSearch = (event) => {
+    if (event.target.value === '') {
+      setFilterList(list);
+      return;
+    }
+    const filteredValues = list.filter(
+      (item) =>
+        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues);
+  };
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <div>
+        Search: <input name='query' type='text' onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item, index) => (
+          <div key={index}>{item}</div> //Display each item
+        ))}
     </div>
   );
 }
